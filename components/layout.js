a2200fac7231a59ab96db118f7fb750fc96e33e6
Ahmed Hassan <ahmedgeo13@gmail.com>
2021-04-07 15:34:14 +0500

    added Background to whole site

--- components/layout.js
+++ components/layout.js
@@ -6,16 +6,25 @@ import { Button } from "../shared/button"
 import * as faSolid from "@emotion-icons/fa-solid"
 import { css, jsx } from "@emotion/react"
 import Link from "next/link"
+import MyParticles from "./particles";
 
 export default function Layout({ children }) {
     return (
         <div>
-
+            <div
+                css={css`
+                  position: fixed;
+                  /* z-index: 0; */
+                  `}
+            >
+                <MyParticles />
+            </div>
             <div
                 css={css`
                   display: flex;
                   justify-content: space-between;
                   padding: 1rem;
+                  position: relative;
                   `}
             >
                 <faSolid.Search
@@ -28,7 +37,9 @@ export default function Layout({ children }) {
                 <Link href="/contact"><Button primary>Contact Us</Button></Link>
             </div>
 
-            <div>{children}</div>
+            <div
+                css={css`
+                  position: relative;`}>{children}</div>
 
             <Sidebar />
 
